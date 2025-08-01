437b0ba96334609bf103259e236069981cb5157e
Gaurav Shambharkar <shambharkargaurav04@gmail.com>
2025-08-01 10:44:55 +0530

    on off toggle

--- frontend/src/Components/DevConsole.jsx
+++ frontend/src/Components/DevConsole.jsx
@@ -8,6 +8,7 @@ export default function DevConsole() {
   const [logs, setLogs] = useState([]);
   const consoleRef = useRef(null);
   const pos = useRef({ x: 100, y: 100 });
+  const [showConsole, setShowConsole] = useState(false);
 
   useEffect(() => {
     socket.on("consoleLog", (data) => {
@@ -36,22 +37,42 @@ export default function DevConsole() {
     document.addEventListener("mouseup", handleMouseUp);
   };
 
+  const handleOnOff = () => {
+    setShowConsole(!showConsole);
+  };
   return (
-    <div
-      ref={consoleRef}
-      className="fixed left-[100px] top-[100px] w-[400px] h-[200px] bg-black bg-opacity-60 text-white text-xs p-2 rounded overflow-auto z-50 shadow-md"
-      style={{ cursor: "move" }}
-      onMouseDown={handleMouseDown}
-    >
-      <div className="font-bold">📟 Dev Console</div>
-      {logs.map((log, i) => (
-        <div
-          key={i}
-          className={log.type === "error" ? "text-red-400" : "text-green-400"}
+    <>
+      <div
+        ref={consoleRef}
+        className={`fixed left-[100px] top-[100px] w-[400px] h-[200px] bg-black bg-opacity-60 text-white text-xs p-2 rounded overflow-auto z-50 shadow-md`}
+        style={{ cursor: "move" }}
+        onMouseDown={handleMouseDown}
+      >
+        <button
+          onClick={handleOnOff}
+          className="bg-white px-3 text-black cursor-pointer rounded-2xl flex items-center font-bold"
+          type="button"
         >
-          {log.message.join(" ")}
+          {showConsole ? "Off" : "On"}
+        </button>
+        <div
+          className={`border  justify-end  ${
+            showConsole ? "hidden" : "block"
+          } `}
+        >
+          <div className="font-bold">📟 Dev Console</div>
+          {logs.map((log, i) => (
+            <div
+              key={i}
+              className={
+                log.type === "error" ? "text-red-400" : "text-green-400"
+              }
+            >
+              {log.message.join(" ")}
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      </div>
+    </>
   );
 }
